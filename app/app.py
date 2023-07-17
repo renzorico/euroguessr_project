7cbc1ca0411c1990e00f55fb91b14744fdbda89b
Renzo Rico <renzorico10@gmail.com>
2023-07-17 12:01:34 +0200

    Deleted pages folder

--- app/app.py
+++ app/app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.write('Welcome to Euroguessr')
+
+def main():
+    # Create a sidebar for navigation
+    st.sidebar.title("Navigation")
+    app_mode = st.sidebar.selectbox("Choose an option", ["Home", "Models", "Results"])
+
+if __name__ == "__main__":
+    main()
